841710e3128ef7b4c1b3f5b19d76c960effb254c
Devin Little <pixlovesit@gmail.com>
2025-07-20 23:40:42 -0400

    fix autostart stuff

---
+++ .config/hypr/autostart.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+### autostart
+
+### setting up varibles
+#config = ~/.config/hypr
+#scripts = $config/scripts
+
+### wallpaper
+#hyprpaper &
+linux-wallpaperengine --screen-root eDP-1 1397593570 &
+
+### idle daemon
+hypridle &
+
+### Noties
+dunst & 
+
+### stuff
+/usr/lib/polkit-kde-authentication-agent-1 & 
+pinentry-gtk & 
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
