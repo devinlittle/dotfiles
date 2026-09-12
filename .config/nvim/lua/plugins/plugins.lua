61f02ac3d606ea93877d6bf07820b2bc1b4fb69f
Devin Little <devinlittlee@gmail.com>
2026-09-12 14:29:03 -0400

    neovim config actually makes sense and is good now

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -6,6 +6,7 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  -- Set Color Scheme as Catppuccin Mocha
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -14,22 +15,17 @@ return {
       flavour = "mocha",
     },
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
     },
   },
-  -- change trouble config
-  {
-    "folke/trouble.nvim",
-    opts = { use_diagnostic_signs = true },
-  },
 
-  -- disable trouble
+  -- disable trouble, it causes trouble...
   { "folke/trouble.nvim", enabled = false },
 
+  -- discord integration
   {
     "IogaMaster/neocord",
     event = "VeryLazy",
@@ -44,206 +40,33 @@ return {
       terminal_text       = "Using Terminal",
     }
   },
-  --!!!  RUST
-  --
-  --
-  {
-    "Saecki/crates.nvim",
-    event = { "BufRead Cargo.toml" },
-    opts = {
-      completion = {
-        cmp = { enabled = true },
-      },
-    },
-  },
---[[{
-  "mrcjkb/rustaceanvim",
-  version = "^9", -- Recommended
-  ft = { "rust" },
-  opts = {
-    server = {
-      on_attach = function(_, bufnr)
-        vim.keymap.set("n", "<leader>cR", function()
-          vim.cmd.RustLsp("codeAction")
-        end, { desc = "Code Action", buffer = bufnr })
-        vim.keymap.set("n", "<leader>dr", function()
-          vim.cmd.RustLsp("debuggables")
-        end, { desc = "Rust Debuggables", buffer = bufnr })
-      end,
-      default_settings = {
-        -- rust-analyzer language server configuration
-        ["rust-analyzer"] = {
-          cargo = {
-            allFeatures = true,
-            loadOutDirsFromCheck = true,
-            buildScripts = {
-              enable = true,
-            },
-          },
-          -- Add clippy lints for Rust.
-          checkOnSave = true,
-          procMacro = {
-            enable = false,
-            ignored = {
-              ["async-trait"] = { "async_trait" },
-              ["napi-derive"] = { "napi" },
-              ["async-recursion"] = { "async_recursion" },
-            },
-          },
-        },
-      },
-    },
-  },
-  config = function(_, opts)
-    vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
-    if vim.fn.executable("rust-analyzer") == 0 then
-      LazyVim.error(
-        "**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
-        { title = "rustaceanvim" }
-      )
-    end
-  end,
-}, ]]
 
-{
-  "mrcjkb/rustaceanvim",
-  version = "^9", -- Recommended
-  ft = { "rust" },
-  lazy = false, -- rustaceanvim handles its own lazy loading via filetypes
-  config = function()
-    vim.g.rustaceanvim = {
-      server = {
-        on_attach = function(_, bufnr)
-          vim.keymap.set("n", "<leader>cR", function()
-            vim.cmd.RustLsp("codeAction")
-          end, { desc = "Code Action", buffer = bufnr })
-          vim.keymap.set("n", "<leader>dr", function()
-            vim.cmd.RustLsp("debuggables")
-          end, { desc = "Rust Debuggables", buffer = bufnr })
-        end,
-        default_settings = {
-          -- rust-analyzer language server configuration
-          ["rust-analyzer"] = {
-            cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              buildScripts = {
-                enable = true,
-              },
-            },
-            check = {
-              command = "clippy",
-            },
-            procMacro = {
-              enable = true, 
-              ignored = {
-                ["napi-derive"] = { "napi" },
-                ["async-recursion"] = { "async_recursion" },
-              },
-            },
-          },
-        },
-      },
-    }
-  end,
-},
- 
- {
-    "j-hui/fidget.nvim",
-    opts = {
-    -- options
-    },
-  },
-  
-  -- override nvim-cmp and add cmp-emoji
- {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    enabled = true,
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, { name = "crates" })
-    end,
-    opts = {
-      experimental = {
-          ghost_text = false
-      }
-    }
-    },
-
-  -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
-  
- -- add pyright to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-          inlay_hints = { enabled = false },
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-	svelte = {},
-        pyright = {},
-	rust_analyzer = { enabled = false },
-      },
-    },
-  },
-
-  -- add tsserver and setup with typescript.nvim instead of lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- tsserver will be automatically installed with mason and loaded with lspconfig
-	svelte = {},
-        tsserver = {},
-	rust_analyzer = { enabled = false },
-      },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-    --[[    -- example to setup with typescript.nvim
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
-        end,]]
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
-    },
-  },
-
-  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+  -- Langauge Specific Features
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.helm" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.svelte" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
 
-  -- add more treesitter parsers
+  -- Disable Inlay Hints
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        clangd = {}, -- i have clangd here because voidlinux is unsupported for mason installtion
+      },
+    },
+  },
+
+  -- Info thinggy in the bottom left
+  { "j-hui/fidget.nvim", },
+
+  -- Auto Install the parsers below if the lazyvim extras didnt do that
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -262,53 +85,25 @@ return {
         "typescript",
         "vim",
         "yaml",
+        "tsx",
+        "typescript",
+        "rust",
+        "ron",
       },
     },
   },
 
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
-  -- If you'd rather extend the default config, use the code below instead:
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
-        "rust",
-        "ron"
-      })
-    end,
-  },
-
-  -- the opts function can also be used to change the default opts:
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "😄")
-    end,
-  },
-
-  -- or you can return new options to override all the defaults
-  {
+  -- INFO: for future
+  --[[ {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function()
       return {
-        --[[add your custom lualine config here]]
       }
     end,
-  },
+  },]]
 
-  -- use mini.starter instead of alpha
- -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
-
-  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
-
-  -- add any tools you want to have installed below
+  -- Scripting LSPs
   {
     "mason-org/mason.nvim",
     opts = {
@@ -317,10 +112,11 @@ return {
         "shellcheck",
         "shfmt",
         "flake8",
---        "rust-analyzer"
       },
     },
   },
+
+  -- LazyGit support inside NeoVim
   {
     "kdheepak/lazygit.nvim",
     cmd = {
@@ -330,14 +126,8 @@ return {
       "LazyGitFilter",
       "LazyGitFilterCurrentFile",
     },
-    -- optional for floating window border decoration
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
   },
 }
